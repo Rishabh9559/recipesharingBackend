7d8a51f2d02e5384ecb4ad29ff4f3b7e4a768d9d
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2024-11-17 12:13:16 +0530

    recipe sharing backend

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:5173' })); // Adjust to your frontend's URL
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Backend starting .... 😉");
+})
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    port: 465,
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    },
+});
+
+app.post('/send-email', (req, res) => {
+    const { to, subject, text, html } = req.body;
+
+    const mailOptions = {
+        from: process.env.GMAIL_USER,
+        to,
+        subject,
+        text,
+        html,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Failed to send email' });
+        }
+        else{
+        res.status(200).json({ message: 'Email sent successfully!', info });
+
+        }
+
+    });
+
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Email server is running on http://localhost:${process.env.PORT}`);
+});
