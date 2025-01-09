ec6c624be8b8d6e414bf6e093eb57a811b145c92
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2025-01-09 12:02:33 +0530

    backend

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import cors from "cors";
 // import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import {otp} from "./otpGenerator.js";
-import {transporter} from "./mail.js";
+import { otp } from "./otpGenerator.js";
+import { transporter } from "./mail.js";
 import { User } from "./models/UserInfo.js";
-import { AddRecipe} from "./models/RecipeInfo.js";
+import { AddRecipe } from "./models/RecipeInfo.js";
 
 dotenv.config();
 const app = express();
@@ -23,55 +23,27 @@ app.get("/", (req, res) => {
   res.send("Backend starting .... 😉");
 });
 
-// Fetch data Lunch
-app.get("/Lunch", async (req,res)=>{
-  const Lunch= await AddRecipe.find({Category:'Lunch'});
-  if(Lunch){
-  res.status(200).json({Lunch:Lunch});
-  }
-  else{
-    res.status(500).json({message:'server error'});
-  }
-  console.log("lunch ");
-  console.log(Lunch);
+// Fetch data recipe
 
+app.get("/Recipe", async (req, res) => {
+  const { Category, limit } = req.query;
+  try {
+    const query = { Category: new RegExp(Category, "i") };
+    const recipes = limit
+      ? await AddRecipe.find(query).limit(parseInt(limit))
+      : await AddRecipe.find(query);
+
+    res.json(recipes);
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
-
-//  fetch data Breakfast
-
-app.get("/Breakfast", async (req,res)=>{
-  const Breakfast=await AddRecipe.find({Category:'Breakfast'});
-  if(Breakfast){
-    res.status(200).json({Breakfast:Breakfast});
-  }
-  else{
-    res.status(500).json({message:'server error'});
-  }
-  console.log(Breakfast);
-})
-
-// fetch data Snack
-app.get("/Snack", async(req,res)=>{
-  const Snack=await AddRecipe.find({Category:'Snack'});
-  if(Snack){
-    res.status(200).json({Snack:Snack});
-  }
-  else{
-    res.status(500).json({message:'server error'});
-  }
-  console.log(Snack);
-
-})
-
-// fetch data Dinner
-app.get("/")
-
-
 
 // Sign Up
 app.post("/SignUp", async (req, res) => {
   const { FirstName, LastName, Email, Password } = req.body;
-//   console.log(req.body);
+  //   console.log(req.body);
   let email = Email.toLowerCase();
   let UserID = "";
   for (let i = 0; i < email.length; i++) {
@@ -84,124 +56,114 @@ app.post("/SignUp", async (req, res) => {
   }
 
   // database store Signup
-  const DublicateUser = await User.findOne({UserID:UserID});
+  const DublicateUser = await User.findOne({ UserID: UserID });
   if (DublicateUser) {
-    console.log(DublicateUser , " Dublicate user");
+    console.log(DublicateUser, " Dublicate user");
     res.status(500).json({ message: "user already aviable please Login" });
-
-
   } else {
-
     const user = await User.create({
-        FirstName: FirstName,
-        LastName: LastName,
-        Email: email,
-        Password: Password,
-        UserID: UserID,
-      });
-      console.log("data store done :");
-      
-      let mailOptions = {
-          from: process.env.GMAIL_USER,
-          to:email,
-          subject:"OTP",
-          text:`One Time Password OTP : ${otp}`,
-          html:` <h2> One Time Password OTP : ${otp} </h2>`,
-        };
-  
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error(error);
-            return res.status(500).json({ message: "Server error try later" });
-          } else {
-            console.log("OTP send successfull",otp);
-            res.status(200).json({ message: otp });
-          }
-        });
+      FirstName: FirstName,
+      LastName: LastName,
+      Email: email,
+      Password: Password,
+      UserID: UserID,
+    });
+    console.log("data store done :");
 
-        
+    let mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: email,
+      subject: "OTP",
+      text: `One Time Password OTP : ${otp}`,
+      html: ` <h2> One Time Password OTP : ${otp} </h2>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error try later" });
+      } else {
+        console.log("OTP send successfull", otp);
+        res.status(200).json({ message: otp });
+      }
+    });
   }
 });
 
 //  Login
 
 app.post("/Login", async (req, res) => {
-
-  const {Email,Password}=req.body;
+  const { Email, Password } = req.body;
   console.log(req.body);
   let email = Email.toLowerCase();
-  const LoginUser= await User.findOne({Email:email, Password:Password})
-  if(LoginUser){
-    res.status(200).json({message:" User found"})
-  }
-
-  else{
-    res.status(500).json({message:"Email or Password wrong or User does not exist"})
+  const LoginUser = await User.findOne({ Email: email, Password: Password });
+  if (LoginUser) {
+    res.status(200).json({ message: " User found" });
+  } else {
+    res
+      .status(500)
+      .json({ message: "Email or Password wrong or User does not exist" });
   }
 });
 
 // Forgot Password
-app.post("/ForgotPassword", async(req,res)=>{
-    const{Email}=req.body;
-    console.log(req.body);
-    let email=Email.toLowerCase();
-    const ForgotPasswordInfo=await User.findOne({Email:email});
-    console.log(ForgotPasswordInfo);
-    if(ForgotPasswordInfo){
-      let mailOptions = {
-        from: process.env.GMAIL_USER,
-        to:email,
-        subject:"Forgot Password",
-        text:`Your Password be : ${ForgotPasswordInfo.Password}`,
-        html:` <h2> Your Password be : ${ForgotPasswordInfo.Password} </h2>`,
-      };
+app.post("/ForgotPassword", async (req, res) => {
+  const { Email } = req.body;
+  console.log(req.body);
+  let email = Email.toLowerCase();
+  const ForgotPasswordInfo = await User.findOne({ Email: email });
+  console.log(ForgotPasswordInfo);
+  if (ForgotPasswordInfo) {
+    let mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: email,
+      subject: "Forgot Password",
+      text: `Your Password be : ${ForgotPasswordInfo.Password}`,
+      html: ` <h2> Your Password be : ${ForgotPasswordInfo.Password} </h2>`,
+    };
 
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error(error);
-          return res.status(500).json({ message: "Server error try later" });
-        } else {
-          console.log("password send to mail");
-          res.status(200).json({message:"Password send to your Email"})
-        }
-      });
-
-
-    }
-    else{
-      res.status(500).json({message:"User does not exist"})
-    }
-})
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error try later" });
+      } else {
+        console.log("password send to mail");
+        res.status(200).json({ message: "Password send to your Email" });
+      }
+    });
+  } else {
+    res.status(500).json({ message: "User does not exist" });
+  }
+});
 
 //  Add Recipe
-app.post("/Add", async(req,res)=>{
-  const{RecipeName,Category,Imageurl,Ingredients,Instructions}=req.body;
-  let UserID="rishabhkushwaha9559";
+app.post("/Add", async (req, res) => {
+  const { RecipeName, Category, Imageurl, Ingredients, Instructions } =
+    req.body;
+  let UserID = "rishabhkushwaha9559";
   console.log(req.body);
- 
-  const RecipeAdd=await AddRecipe.create({
-    UserID:UserID,
-    RecipeName:RecipeName,
-    Category:Category,
-    Imageurl:Imageurl,
-    Ingredients:Ingredients,
-    Instructions:Instructions,
 
+  const RecipeAdd = await AddRecipe.create({
+    UserID: UserID,
+    RecipeName: RecipeName,
+    Category: Category,
+    Imageurl: Imageurl,
+    Ingredients: Ingredients,
+    Instructions: Instructions,
   });
-  if(RecipeAdd){
-    res.status(200).json({message:"Recipe add successful"});
+  if (RecipeAdd) {
+    res.status(200).json({ message: "Recipe add successful" });
+  } else {
+    res
+      .status(500)
+      .json({ message: "some thing went wrong, please try later" });
   }
-  else{
-    res.status(500).json({message:"some thing went wrong, please try later"});
-  }
-  
-})
-
+});
 
 // Email Sending
 app.post("/send-email", (req, res) => {
   const { to, subject, text, html } = req.body;
-    console.log(req.body);
+  console.log(req.body);
 
   let mailOptions = {
     from: process.env.GMAIL_USER,
