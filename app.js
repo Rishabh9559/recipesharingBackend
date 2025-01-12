48f2f4037c918f24c2888051ff1515fd6ea54785
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2025-01-12 19:34:35 +0530

    b

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { otp } from "./otpGenerator.js";
 import { transporter } from "./mail.js";
 import { User } from "./models/UserInfo.js";
 import { AddRecipe } from "./models/RecipeInfo.js";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -15,8 +17,9 @@ mongoose.connect("mongodb://localhost:27017/RecipeSharing").then(() => {
   console.log("data base connected");
 });
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Backend starting .... 😉");
@@ -39,26 +42,22 @@ app.get("/Recipe", async (req, res) => {
   }
 });
 
-// getch by id 
-app.get("/show", async (req,res)=>{
-  const {id}=req.query;
-   const showRecipe= await AddRecipe.find({_id:id});
-   if (showRecipe){
-  res.json(showRecipe);
-   }
-   else{
-    res.status(500).json({message:'server error'});
-   }
-
-})
-
+// getch by id
+app.get("/show", async (req, res) => {
+  const { id } = req.query;
+  const showRecipe = await AddRecipe.find({ _id: id });
+  if (showRecipe) {
+    res.json(showRecipe);
+  } else {
+    res.status(500).json({ message: "server error" });
+  }
+});
 
 // Sign Up
 app.post("/SignUp", async (req, res) => {
   const { FirstName, LastName, Email, Password } = req.body;
   //   console.log(req.body);
-let UserID = Email.toLowerCase().split("@")[0];
- 
+  let UserID = Email.toLowerCase().split("@")[0];
 
   // database store Signup
   const DublicateUser = await User.findOne({ UserID: UserID });
@@ -66,7 +65,6 @@ let UserID = Email.toLowerCase().split("@")[0];
     console.log(DublicateUser, " Dublicate user");
     res.status(500).json({ message: "user already aviable please Login" });
   } else {
-
     let mailOptions = {
       from: process.env.GMAIL_USER,
       to: Email,
@@ -96,7 +94,6 @@ let UserID = Email.toLowerCase().split("@")[0];
       });
       console.log("data store done :");
     }
-
   }
 });
 
@@ -105,29 +102,37 @@ let UserID = Email.toLowerCase().split("@")[0];
 app.post("/Login", async (req, res) => {
   const { Email, Password } = req.body;
   console.log(req.body);
+  if (!Email || !Password) {
+    return res.status(400).json({ message: "Email and Password Required" });
+  }
   const LoginUser = await User.findOne({ Email: Email.toLowerCase() });
-  
-  if(LoginUser){
 
-    const isPasswordValid= await LoginUser.isPasswordCorrect(Password);
-  
+  if (LoginUser) {
+    const isPasswordValid = await LoginUser.isPasswordCorrect(Password);
+
     if (isPasswordValid) {
-      res.status(200).json({ message: " User found" });
+      const accessToken = await LoginUser.generateAccessToken();
+      console.log("Access token : ", accessToken);
+      const options = {
+        httpOnly: true,
+        secure: false,
+      };
+
+      return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .json({ message: "Login successfully" });
     } else {
-      res
-        .status(401)
-        .json({ message: "Incorrect Password " });
+      res.status(401).json({ message: "Incorrect Password " });
     }
+  } else {
+    res.status(401).json({ message: "Email Not Valid" });
   }
+});
 
-  else{
-   
-      res.status(401).json({message:"Email Not Valid"});
-
-  }
-
-
-
+// LogOut
+app.post("/logout", (req, res) => {
+  res.clearCookie("accessToken").json({ message: "Logged out successfully" });
 });
 
 // Forgot Password
@@ -144,7 +149,6 @@ app.post("/ForgotPassword", async (req, res) => {
     return res.status(404).json({ message: "User does not exist." });
   }
 
-
   let mailOptions = {
     from: process.env.GMAIL_USER,
     to: Email,
@@ -155,27 +159,69 @@ app.post("/ForgotPassword", async (req, res) => {
 
   transporter.sendMail(mailOptions, async (error, info) => {
     if (error) {
-      return res.status(500).json({ message: "Failed to send email. Try again later." });
+      return res
+        .status(500)
+        .json({ message: "Failed to send email. Try again later." });
     }
-    console.log("new password: ",otp);
-    ForgotPasswordInfo.Password = otp; 
+    console.log("new password: ", otp);
+    ForgotPasswordInfo.Password = otp;
     try {
       await ForgotPasswordInfo.save();
       res.status(200).json({ message: "New password sent to your email." });
     } catch (err) {
-      res.status(500).json({ message: "Failed to update password. Try again later." });
+      res
+        .status(500)
+        .json({ message: "Failed to update password. Try again later." });
     }
   });
 });
 
+// profile
 
+app.get("/profile", async (req, res) => {
+  console.log("request cookies :", req.cookies);
 
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log("decode access token :", decoded);
+    const user = await User.findById(decoded._id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({
+      UserID: user.UserID,
+      FirstName: user.FirstName,
+      LastName: user.LastName,
+      Email: user.Email,
+    });
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+    console.log(error);
+  }
+});
+
+// post fetch
+app.get("/post", async (req, res) => {
+  const { UserID } = req.query;
+  const showRecipe = await AddRecipe.find({ UserID:UserID });
+  if (showRecipe) {
+    res.json(showRecipe);
+  } else {
+    res.status(500).json({ message: "server error" });
+  }
+});
 
 //  Add Recipe
 app.post("/Add", async (req, res) => {
-  const { RecipeName, Category, Imageurl, Ingredients, Instructions } =
+  const { RecipeName, Category, Imageurl, Ingredients, Instructions, UserID } =
     req.body;
-  let UserID = "rishabhkushwaha9559";
+  // let UserID = "rishabhkushwaha9559";
   console.log(req.body);
 
   const RecipeAdd = await AddRecipe.create({
--- models/UserInfo.js
+++ models/UserInfo.js
@@ -2,32 +2,46 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const UserInfo=new Schema({
-    
-    FirstName:{type:String,require:true},
-    LastName:{type:String, require:true},
-    Email:{type:String,require:true, unique:true},
-    Password:{type:String,require:true},
-    UserID:{type:String, require:true, unique:true, index:true, lowercase:true},
+const UserInfo = new Schema({
+  FirstName: { type: String, require: true },
+  LastName: { type: String, require: true },
+  Email: { type: String, require: true, unique: true },
+  Password: { type: String, require: true },
+  UserID: {
+    type: String,
+    require: true,
+    unique: true,
+    index: true,
+    lowercase: true,
+  },
+});
 
-})
+UserInfo.pre("save", async function (next) {
+  if (!this.isModified("Password")) return next();
+  this.Password = await bcrypt.hash(this.Password, 10);
+  next();
+});
 
-UserInfo.pre("save", async function (next){
-    if(!this.isModified("Password"))
-        return next();
-    this.Password=await bcrypt.hash(this.Password,10);
-    next();
-})
+UserInfo.methods.isPasswordCorrect = async function (Password) {
+  return await bcrypt.compare(Password, this.Password);
+};
 
-UserInfo.methods.isPasswordCorrect=async function(Password){
-    return await bcrypt.compare(Password,this.Password)
-}
+UserInfo.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      Email: this.Email,
+      UserID: this.UserID,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-UserInfo.methods.generateAccessToken=
+const User = mongoose.model("UserInfo", UserInfo);
 
-
-const User=mongoose.model('UserInfo',UserInfo);
-
-export {User};
+export { User };
